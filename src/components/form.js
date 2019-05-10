e82baef548c789ea65d17fd476a2d7e2948d256a
BintangSaputra <bintangcbs26@gmail.com>
2019-05-10 11:09:00 +0700

    add handleOnChange function

--- src/components/form.js
+++ src/components/form.js
@@ -3,13 +3,35 @@ import React, { Component } from "react";
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      description: "",
+      input: []
+    };
+
+    handleOnChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    };
+
+    handleOnSubmit = () => {
+      const Data = {
+        input: this.state.description
+      };
+
+      this.setState({ input: this.state.input.concat(Data) });
+    };
   }
   render() {
     return (
       <div>
-        <input type="text" placeholder="todo" name="description" />
-        <button>Submit</button>
+        <input
+          type="text"
+          placeholder="todo"
+          name="description"
+          onChange={this.handleOnChange}
+        />
+        <button onClick={handleOnSubmit}>Submit</button>
       </div>
     );
   }
